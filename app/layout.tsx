2a7a5809876bc0f0d13ed7c5d383ced4b2b6a400
s a s w a t <151541676+sashhh31@users.noreply.github.com>
2025-03-27 10:58:41 +0530

    google ads added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,16 @@ import { Inter } from 'next/font/google'
 import { ProfileProvider } from '../context/ProfileContext';
 import './globals.css'
 import { Header } from '@/components/header'
-
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "KnowAI - AI Career Coach",
-  
 }
+
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 })
-
 
 export default function RootLayout({
   children,
@@ -24,6 +23,25 @@ export default function RootLayout({
     <ProfileProvider>
       <html lang="en">
         <body className={`${inter.variable} antialiased`}>
+          {/* Google Tag Manager Script */}
+          <Script 
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-16956742446"
+            strategy="afterInteractive"
+          />
+          <Script 
+            id="google-ads-config"
+            strategy="afterInteractive"
+          >
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16956742446');
+              });
+            `}
+          </Script>
+          
           <Header />
           {children}
         </body>
